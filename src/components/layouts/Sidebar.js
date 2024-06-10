cc5e877956b72c1652e29ca166d9f2dd1cd25344
suraj suwal <surajsuwa@gmail.com>
2024-06-10 22:34:32 +0545

    Fixed card design

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -1,27 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import home from '@iconify-icons/mdi-light/home';
 
 const Sidebar = () => {
   return (
     <>
       <div className="bg-gray-900 px-2 lg:px-4 py-2 lg:py-10 sm:rounded-xl flex lg:flex-col justify-between">
         <nav className="flex items-center flex-row space-x-2 lg:space-x-0 lg:flex-col lg:space-y-2">
+          {/* Home */}
           <a className="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-6 sm:w-6" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-            </svg>
+            <Icon icon={home} width={32} height={32} />
           </a>
 
+
+          {/* Service */}
           <a className="bg-gray-800 text-white p-4 inline-flex justify-center rounded-md" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-6 sm:w-6" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-            </svg>
+            <Icon icon="carbon:web-services-container" width={32} height={32} />
           </a>
-          <a className="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
+
+          {/* People */}
+          <a className=" text-white p-4 inline-flex justify-center rounded-md" href="#">
+            <Icon icon="ion:people-outline" width={32} height={32} />
+          </a>
+
+
+          {/* <a className="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 sm:h-6 sm:w-6" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
             </svg>
-          </a>
+          </a> */}
         </nav>
         <div className="flex items-center flex-row space-x-2 lg:space-x-0 lg:flex-col lg:space-y-2">
           <a className="text-white/50 p-4 inline-flex justify-center rounded-md hover:bg-gray-800 hover:text-white smooth-hover" href="#">
--- src/pages/Dashboard/DashboardPage.js
+++ src/pages/Dashboard/DashboardPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import profileImage from '../../assets/images/users/avatar.jpg';
 
 function DashboardPage() {
     return (
@@ -10,49 +11,49 @@ function DashboardPage() {
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                         </svg>
                     </a>
-                    <a className="text-white/50 group-hover:text-white group-hover:smooth-hover text-center" href="#">Create group</a>
+                    <a className="text-white/50 group-hover:text-white group-hover:smooth-hover text-center" href="#">Add New User</a>
                 </div>
                 <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-                    <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1547592180-85f173990554?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="cuisine" />
-                    <h4 className="text-white text-2xl font-bold capitalize text-center">Cuisine</h4>
+                    <img className="w-20 h-20 object-cover object-center rounded-full" src={profileImage} alt="user" />
+                    <h4 className="text-white text-2xl font-bold capitalize text-center">Rahul Pandey</h4>
                     <p className="text-white/50">55 members</p>
-                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">22 Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
+                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
                 </div>
                 <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-                    <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1513364776144-60967b0f800f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80" alt="art" />
-                    <h4 className="text-white text-2xl font-bold capitalize text-center">Art</h4>
+                    <img className="w-20 h-20 object-cover object-center rounded-full" src={profileImage} alt="user" />
+                    <h4 className="text-white text-2xl font-bold capitalize text-center">Gaurav</h4>
                     <p className="text-white/50">132 members</p>
-                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">4 Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
+                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
                 </div>
                 <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-                    <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1560419015-7c427e8ae5ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="gaming" />
-                    <h4 className="text-white text-2xl font-bold capitalize text-center">Gaming</h4>
+                    <img className="w-20 h-20 object-cover object-center rounded-full" src={profileImage} alt="user" />
+                    <h4 className="text-white text-2xl font-bold capitalize text-center">Sarita</h4>
                     <p className="text-white/50">207 members</p>
-                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">0 Online <span className="ml-2 w-2 h-2 block bg-red-400 rounded-full group-hover:animate-pulse"></span></p>
+                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">offline<span className="ml-2 w-2 h-2 block bg-red-400 rounded-full group-hover:animate-pulse"></span></p>
                 </div>
                 <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-                    <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80" alt="cinema" />
-                    <h4 className="text-white text-2xl font-bold capitalize text-center">cinema</h4>
+                    <img className="w-20 h-20 object-cover object-center rounded-full" src={profileImage} alt="user" />
+                    <h4 className="text-white text-2xl font-bold capitalize text-center">Avin</h4>
                     <p className="text-white/50">105 members</p>
-                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">12 Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
+                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
                 </div>
                 <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-                    <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1484704849700-f032a568e944?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="song" />
-                    <h4 className="text-white text-2xl font-bold capitalize text-center">Song</h4>
+                    <img className="w-20 h-20 object-cover object-center rounded-full" src={profileImage} alt="user" />
+                    <h4 className="text-white text-2xl font-bold capitalize text-center">Suraya</h4>
                     <p className="text-white/50">67 members</p>
-                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">3 Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
+                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
                 </div>
                 <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-                    <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="code" />
-                    <h4 className="text-white text-2xl font-bold capitalize text-center">Code</h4>
+                    <img className="w-20 h-20 object-cover object-center rounded-full" src={profileImage} alt="user" />
+                    <h4 className="text-white text-2xl font-bold capitalize text-center">Gautam</h4>
                     <p className="text-white/50">83 members</p>
-                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">43 Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
+                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
                 </div>
                 <div className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
-                    <img className="w-20 h-20 object-cover object-center rounded-full" src="https://images.unsplash.com/photo-1533147670608-2a2f9775d3a4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="dancing" />
-                    <h4 className="text-white text-2xl font-bold capitalize text-center">Dancing</h4>
+                    <img className="w-20 h-20 object-cover object-center rounded-full" src={profileImage} alt="user" />
+                    <h4 className="text-white text-2xl font-bold capitalize text-center">Preety</h4>
                     <p className="text-white/50">108 members</p>
-                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">86 Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
+                    <p className="absolute top-2 text-white/20 inline-flex items-center text-xs">Online <span className="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
                 </div>
             </div>
         </>
