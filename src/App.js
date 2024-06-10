8193cd3663e3f75f2c03a311adb1624aeb3950f4
suraj suwal <surajsuwa@gmail.com>
2024-06-10 22:02:57 +0545

    removed unwanted layouts

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import React from 'react';
 import AppRoutes from './routes'; // Import the AppRoutes component
 import Header from './components/layouts/Header';
 import Sidebar from './components/layouts/Sidebar';
+// import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <div className="bg-gray-900 min-h-screen flex items-center justify-center">
-        <div className="bg-gray-800 flex-1 flex flex-col space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10 max-w-6xl sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
+        <div className="bg-gray-800 flex-1 flex flex-col space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10 max-w-6xl sm:p-6 sm:my-10 sm:mx-4 sm:rounded-2xl">
           {/* sidebar */}
           <Sidebar />
           <div className="flex-1 px-2 sm:px-0">
@@ -17,6 +18,7 @@ function App() {
             <Header />
             {/* Main Body */}
             <AppRoutes /> {/* Render the AppRoutes component */}
+            {/* <Outlet /> */}
           </div>
         </div>
       </div>
