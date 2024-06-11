cfd98ae7f2516adf90916fac4c8c644f2f5958f3
suraj suwal <surajsuwa@gmail.com>
2024-06-11 09:38:43 +0545

    add comment on app.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
             <Sidebar />
             <div className="flex-1 px-2 sm:px-0">
               <Header />
+              {/* Main Container */}
               <AppRoutes />
             </div>
           </div>
