ec7f369e2a763ccf543d135a5a83ece97dcb8b0e
suraj suwal <surajsuwa@gmail.com>
2024-06-12 08:04:13 +0545

    removed unwanted comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes'; // Import the AppRoutes component
+import AppRoutes from './routes'; 
 import Header from './components/layouts/Header';
 import Sidebar from './components/layouts/Sidebar';
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 // routes.js
 
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Remove BrowserRouter import
+import { Routes, Route } from 'react-router-dom'; 
 import DashboardPage from './pages/Dashboard/DashboardPage';
 import UserPage from './pages/User/UserPage';
 import BudgetPage from './pages/Budgets/BudgetPage';
@@ -9,10 +9,9 @@ import BudgetPage from './pages/Budgets/BudgetPage';
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<DashboardPage />} /> {/* Render DashboardPage for the root path */}
-      <Route path="/user" element={<UserPage />} /> {/* Render UserPage for the /user path */}
+      <Route path="/" element={<DashboardPage />} /> 
+      <Route path="/user" element={<UserPage />} /> 
       <Route path="/budget" element={<BudgetPage />} />
-      {/* Add more routes here if needed */}
     </Routes>
   );
 };
