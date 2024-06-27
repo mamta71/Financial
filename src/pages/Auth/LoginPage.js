7c85db03617ed7579e911a55f3f653431128c294
mamta71 <kmamata19@gmail.com>
2024-06-27 15:53:07 +0545

    fix: fix app.js

--- src/pages/Auth/LoginPage.js
+++ src/pages/Auth/LoginPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const LoginPage = () => {
   return (
     <div className="flex justify-center">
@@ -49,7 +50,7 @@ const LoginPage = () => {
             type="submit"
             className=" w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Save
+           Login
           </button>
 
           <div className="mt-3">
